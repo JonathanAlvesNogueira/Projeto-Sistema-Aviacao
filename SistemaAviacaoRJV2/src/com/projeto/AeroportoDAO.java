5796d49607ae576eb08b3b207101f64646a95f27
Ronan Jorge <101803856+RonanJorge@users.noreply.github.com>
2023-12-06 21:35:30 -0300

    Delete SistemaAviacaoRJV2 directory

--- SistemaAviacaoRJV2/src/com/projeto/AeroportoDAO.java
+++
@@ -1,9 +0,0 @@
-package com.projeto;
-
-import java.util.List;
-
-public interface AeroportoDAO {
-	void cadastrar(Aeroporto a);
-	List<Aeroporto> consultarTodos();
-	List<Aeroporto> consultarNome(String nome);
-}
--- SistemaAviacaoRJV2/src/com/projeto/BoundaryRender.java
+++
@@ -1,8 +0,0 @@
-package com.projeto;
-
-import javafx.scene.layout.Pane;
-
-public interface BoundaryRender {
-	@SuppressWarnings("exports")
-	Pane render();
-}
